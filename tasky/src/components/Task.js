cd5618d9569cbe8542c3fc9be898497cb917a5c3
Shijin Zhang <1364226740@qq.com>
2023-09-26 20:29:11 +0800

    Revert "Week 3 Lab A done"
    This reverts commit 47f020abe1d9c5d1195c14f0e7ab45607d05fa78.

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Task = (props) => {
     return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+        <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
             <p>{props.children}</p>
-            <button className='doneButton' onClick={props.markDone}>Done</button>
-            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     );
 };
